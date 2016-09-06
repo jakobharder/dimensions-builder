5bf973c48996e96608fe54537d4df18a1571952b
Jakob Harder <jharder@live.com>
2016-09-06 19:12:55 +0200

    Removed too much whitespace from routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,51 +13,16 @@ import {
 } from './pages/index';
 import { TeamBuilderComponent } from './team-builder/index';
 
-
 export const routes: RouterConfig = [
-	{
-		path: '',
-		pathMatch: 'full',
-		component: HomeComponent
-	},
-	{
-		path: 'all-waves-and-packs',
-		component: WaveListComponent
-	},
-	{
-		path: 'wave/:id',
-		component: WaveDetailsComponent
-	},
-	{
-		path: 'pack/:id',
-		component: PackDetailsComponent
-	},
-	{
-		path: 'characters',
-		component: MinifigListComponent
-	},
-	{
-		path: 'character/:id',
-		component: MinifigDetailsComponent
-	},
-	{
-		path: 'abilities',
-		component: AbilityListComponent
-	},
-	{
-		path: 'ability/:id',
-		component: AbilityDetailsComponent
-	},
-	{
-		path: 'team-builder',
-		component: TeamBuilderComponent
-	},
-	{
-		path: 'level/:id',
-		component: LevelDetailsComponent
-	},
-	{
-		path: '**',
-		redirectTo: '/'
-	}
+	{ path: '', component: HomeComponent, pathMatch: 'full' },
+	{ path: 'all-waves-and-packs', component: WaveListComponent },
+	{ path: 'wave/:id', component: WaveDetailsComponent },
+	{ path: 'pack/:id', component: PackDetailsComponent },
+	{ path: 'characters', component: MinifigListComponent },
+	{ path: 'character/:id', component: MinifigDetailsComponent },
+	{ path: 'abilities', component: AbilityListComponent },
+	{ path: 'ability/:id', component: AbilityDetailsComponent },
+	{ path: 'team-builder', component: TeamBuilderComponent },
+	{ path: 'level/:id', component: LevelDetailsComponent },
+	{ path: '**', redirectTo: '/' }
 ];
