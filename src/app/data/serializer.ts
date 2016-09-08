13d669bb46a8ff553e94fa0e87149df43fddb114
Jakob Harder <jharder@live.com>
2016-09-08 17:34:43 +0200

    Changed serializer to base64

--- src/app/data/serializer.ts
+++ src/app/data/serializer.ts
@@ -1,13 +1,13 @@
 import { Abilities } from './ability';
 
 export class Serializer {
+    private rixit = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_";
+
     abilitiesToString(abilities: Abilities) {
         let output = "";
         for (let skill of abilities.list) {
-            if (skill.id < 25) {
-                output += String.fromCharCode(skill.id + 65);
-            } else if (skill.id < 50) {
-                output += String.fromCharCode(skill.id + 97 - 25);
+            if (skill.id < 64) {
+                output += this.rixit.charAt(skill.id);
             } else {
                 // TODO
             }
@@ -18,11 +18,9 @@ export class Serializer {
     stringToAbilities(query: string) {
         let abilities: number[] = [];
         for (let i = 0; i < query.length; i++) {
-            let char = query.charCodeAt(i);
-            if (char >= 65 && char <= 90) {
-                abilities.push(char - 65);
-            } else if (char >= 97 && char <= 122) {
-                abilities.push(char - 97 + 25);
+            let index = this.rixit.indexOf(query.charAt(i));
+            if (index != -1) {
+                abilities.push(index);
             } else {
                 // TODO
             }
