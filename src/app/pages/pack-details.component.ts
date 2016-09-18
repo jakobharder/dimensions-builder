af6d62a82d3f48acfbbae2dd0cb1664d019752f9
Jakob Harder <jharder@live.com>
2016-09-18 14:20:20 +0200

    updated pack details

--- src/app/pages/pack-details.component.ts
+++ src/app/pages/pack-details.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Title } from '@angular/platform-browser';
+import { MetaService } from '../meta';
 import { ROUTER_DIRECTIVES, ActivatedRoute } from '@angular/router';
-import { Pack, Minifig, Skill, DataService } from '../data/index';
-import { MinifigPanelComponent } from '../components/index';
+import { Pack, Minifig, Skill, DataService, packTypeStrings } from '../data';
+import { MinifigPanelComponent } from '../components';
 
 @Component({
 	moduleId: module.id,
 	selector: 'pack-details',
+    styleUrls: ['pack-details.component.css'],
 	templateUrl: 'pack-details.component.html',
     styles: ['img { max-width: 100%; }'],
     directives: [ROUTER_DIRECTIVES, MinifigPanelComponent]
@@ -14,24 +15,34 @@ import { MinifigPanelComponent } from '../components/index';
 export class PackDetailsComponent implements OnInit, OnDestroy {
     sub: any;
     pack: Pack;
-    minifigs: Minifig[];
+    characters: Minifig[];
     skills: Skill[];
+    private type: string;
 
     constructor(private route: ActivatedRoute,
                 private dataService: DataService,
-                private title: Title) {
+                private meta: MetaService) {
 
     }
 
     ngOnInit() {
-        this.title.setTitle("Pack Details - Lego Dimensions Builder"); 
+        this.meta.set({
+            title: "Pack Details",
+            description: ""
+        });
+
         this.sub = this.route.params.subscribe(params => {
             let id = +params['id'];
             this.pack = this.dataService.getPack(id);
-            this.minifigs = this.dataService.getMinifigs(this.pack.minifigs);
+            this.characters = this.dataService.getMinifigs(this.pack.minifigs);
+            console.log(this.characters);
 
-            this.skills = this.dataService.getSkills(this.minifigs.getSkills());
-            this.title.setTitle(this.pack.name + " Details - Lego Dimensions Builder");
+            this.skills = this.dataService.getSkills(this.characters.getSkills());
+            this.meta.set({
+                title: this.pack.name + " " + packTypeStrings[this.pack.type],
+                description: ""
+            });
+            this.type = packTypeStrings[this.pack.type];
         });
     }
 
