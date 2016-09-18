ed27424bcc1b48679b37102c24c0e231b2d4a23a
Jakob Harder <jharder@live.com>
2016-09-18 20:58:09 +0200

    updated character list page

---
+++ src/app/pages/character-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+import { MetaService } from '../meta';
+import { Piece, DataService, Pieces } from '../data';
+import { PieceTableComponent } from '../components/tables';
+
+@Component({
+	moduleId: module.id,
+	selector: 'character-list',
+	templateUrl: 'character-list.component.html',
+    directives: [ROUTER_DIRECTIVES, PieceTableComponent ]
+})
+
+export class CharacterListComponent implements OnInit {
+    characters: Piece[];
+
+    constructor(private dataService: DataService,
+                private meta: MetaService) {
+    }
+
+    ngOnInit() {
+        this.meta.set({
+            title: "Complete list of all characters",
+            description: "Overview of all Lego Dimensions characters."
+        });
+        this.characters = new Pieces(this.dataService.getAllMinifigs()).getOrdered().byName().list;
+    }
+}
--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -1,6 +1,6 @@
 export * from './wave-list.component';
 export * from './pack-details.component';
-export * from './minifig-list.component';
+export * from './character-list.component';
 export * from './minifig-details.component';
 export * from './ability-list.component';
 export * from './home.component';
