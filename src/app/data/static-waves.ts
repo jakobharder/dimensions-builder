cbcbb278a1f8ea974f5747d2ed93d3eebd653259
Jakob Harder <jharder@live.com>
2016-09-21 19:40:08 +0200

    fixed wave years

--- src/app/data/static-waves.ts
+++ src/app/data/static-waves.ts
@@ -6,12 +6,12 @@ export class WaveData {
 }
 
 export const waves: WaveData[] = [
-    { year: 1, wave: 1, release: "September 2016" },
-    { year: 1, wave: 2, release: "November 2016" },
+    { year: 1, wave: 1, release: "September 2015" },
+    { year: 1, wave: 2, release: "November 2015" },
     { year: 1, wave: 3, release: "January 2016" },
     { year: 1, wave: 4, release: "March 2016" },
     { year: 1, wave: 5, release: "May 2016" },
     { year: 2, wave: 6, release: "September 2016" },
     { year: 2, wave: 7, release: "November 2016" },
-    { year: 2, wave: 7.5, release: "February 2016" },
+    { year: 2, wave: 7.5, release: "February 2017" },
 ];
