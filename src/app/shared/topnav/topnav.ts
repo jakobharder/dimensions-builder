79bafd079a5a68c2a5dbbdde28cf8a22ff43de6a
Jakob Harder <jharder@live.com>
2016-09-24 16:03:55 +0200

    styled topnav

--- src/app/shared/topnav/topnav.ts
+++ src/app/shared/topnav/topnav.ts
@@ -7,28 +7,11 @@ import { CORE_DIRECTIVES } from '@angular/common';
     moduleId: module.id,
     selector: 'top-nav',
     templateUrl: 'topnav.html',
+	styleUrls: ['topnav.css'],
     encapsulation: ViewEncapsulation.None,
-    directives: [/*DROPDOWN_DIRECTIVES, */CORE_DIRECTIVES, ROUTER_DIRECTIVES]
+    directives: [CORE_DIRECTIVES, ROUTER_DIRECTIVES]
 })
 
 export class TopNavComponent {
-	changeTheme(color: string): void {
-		var link: any = $('<link>');
-		link
-			.appendTo('head')
-			.attr({type : 'text/css', rel : 'stylesheet'})
-			.attr('href', 'themes/app-'+color+'.css');
-	}
 
-	rtl(): void {
-		var body: any = $('body');
-		body.toggleClass('rtl');
-	}
-
-	sidebarToggler(): void  {
-		var sidebar: any = $('#sidebar');
-		var mainContainer: any = $('.main-container');
-		sidebar.toggleClass('sidebar-left-zero');
-		mainContainer.toggleClass('main-container-ml-zero');
-	}
 }
