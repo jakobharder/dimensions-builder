262ab1f616ec015ff3a79c0e625a0cc612d50cad
Jakob Harder <jharder@live.com>
2016-09-24 10:52:02 +0200

    added all story and pack levels

--- src/app/data/static-levels.ts
+++ src/app/data/static-levels.ts
@@ -22,5 +22,60 @@ export const levels: LevelData[] = [
         walkthrough: 'http://www.ign.com/wikis/lego-dimensions/Follow_the_LEGO_Brick_Road' },
 
     { url: 'main-story-2-meltdown-at-sector-7g', name: 'Meltdown at Sector 7-G – The Simpsons', chapter: 2, story: 'main story',
-        abilitiesMinikits: [], abilitiesRescue: [], abilitiesExtra: [], abilitiesStory: [], walkthrough: '' }
+        abilitiesMinikits: [], abilitiesRescue: [], abilitiesExtra: [], abilitiesStory: [], walkthrough: '' },
+
+    { url: 'main-story-3-elements-of-surprise', name: 'Elements of Surprise', chapter: 3, story: 'main story',
+        abilitiesMinikits: [], abilitiesRescue: [], abilitiesExtra: [], abilitiesStory: [], walkthrough: '' },
+
+    { url: 'main-story-4-dalektable-adventure', name: 'A Dalektable Adventure', chapter: 4, story: 'main story',
+        abilitiesMinikits: [], abilitiesRescue: [], abilitiesExtra: [], abilitiesStory: [], walkthrough: '' },
+
+    { url: 'main-story-5-painting-the-town-black', name: 'Painting the Town Black', chapter: 5, story: 'main story',
+        abilitiesMinikits: [], abilitiesRescue: [], abilitiesExtra: [], abilitiesStory: [], walkthrough: '' },
+
+    { url: 'main-story-6-once-upon-a-time-machine-in-the-west', name: 'Once Upon A Time Machine in the West', chapter: 6, story: 'main story',
+        abilitiesMinikits: [], abilitiesRescue: [], abilitiesExtra: [], abilitiesStory: [], walkthrough: '' },
+
+    { url: 'main-story-7-glad-to-see-you', name: 'GLaD to See You', chapter: 7, story: 'main story',
+        abilitiesMinikits: [], abilitiesRescue: [], abilitiesExtra: [], abilitiesStory: [], walkthrough: '' },
+
+    { url: 'main-story-8-riddle-earth', name: 'Riddle-earth', chapter: 8, story: 'main story',
+        abilitiesMinikits: [], abilitiesRescue: [], abilitiesExtra: [], abilitiesStory: [], walkthrough: '' },
+
+    { url: 'main-story-9-the-phantom-zone', name: 'The Phantom Zone', chapter: 9, story: 'main story',
+        abilitiesMinikits: [], abilitiesRescue: [], abilitiesExtra: [], abilitiesStory: [], walkthrough: '' },
+
+    { url: 'main-story-10-all-your-bricks-are-belong-to-us', name: 'All Your Bricks Are Belong To Us', chapter: 10, story: 'main story',
+        abilitiesMinikits: [], abilitiesRescue: [], abilitiesExtra: [], abilitiesStory: [], walkthrough: '' },
+
+    { url: 'main-story-11-mystery-mansion-mash-up', name: 'Mystery Mansion Mash-Up', chapter: 11, story: 'main story',
+        abilitiesMinikits: [], abilitiesRescue: [], abilitiesExtra: [], abilitiesStory: [], walkthrough: '' },
+
+    { url: 'main-story-12-prime-time', name: 'Prime Time', chapter: 12, story: 'main story',
+        abilitiesMinikits: [], abilitiesRescue: [], abilitiesExtra: [], abilitiesStory: [], walkthrough: '' },
+
+    { url: 'main-story-13-the-end-is-tri', name: 'The End is Tri', chapter: 13, story: 'main story',
+        abilitiesMinikits: [], abilitiesRescue: [], abilitiesExtra: [], abilitiesStory: [], walkthrough: '' },
+
+    { url: 'main-story-14-the-final-dimension', name: 'The Final Dimension', chapter: 14, story: 'main story',
+        abilitiesMinikits: [], abilitiesRescue: [], abilitiesExtra: [], abilitiesStory: [], walkthrough: '' },
+
+    { url: 'back-to-the-future-level', name: 'A Hill Valley Time Travel Adventure', chapter: 0, story: 'level pack',
+        abilitiesMinikits: [], abilitiesRescue: [], abilitiesExtra: [], abilitiesStory: [], walkthrough: '' },
+
+    { url: 'portal-2-level', name: 'Aperture Science', chapter: 0, story: 'level pack',
+        abilitiesMinikits: [], abilitiesRescue: [], abilitiesExtra: [], abilitiesStory: [], walkthrough: '' },
+
+    { url: 'doctor-who-level', name: 'An Adventure in Space and Time', chapter: 0, story: 'level pack',
+        abilitiesMinikits: [], abilitiesRescue: [], abilitiesExtra: [], abilitiesStory: [], walkthrough: '' },
+
+    { url: 'the-simpsons-level', name: 'The Mysterious Voyage of Homer', chapter: 0, story: 'level pack',
+        abilitiesMinikits: [], abilitiesRescue: [], abilitiesExtra: [], abilitiesStory: [], walkthrough: '' },
+
+    { url: 'ghostbusers-level', name: 'Ghostbusers Level', chapter: 0, story: 'level pack',
+        abilitiesMinikits: [], abilitiesRescue: [], abilitiesExtra: [], abilitiesStory: [], walkthrough: '' },
+
+    { url: 'midway-arcade-level', name: 'Midway Arcade Level', chapter: 0, story: 'level pack',
+        abilitiesMinikits: [], abilitiesRescue: [], abilitiesExtra: [], abilitiesStory: [], walkthrough: '' },
+
 ];
