f772962fc437f6c17bf0c4699ac281f63d727947
Jakob Harder <jharder@live.com>
2016-10-01 18:09:21 +0200

    added franchise data to levels

--- src/app/data/data-types.ts
+++ src/app/data/data-types.ts
@@ -19,3 +19,47 @@ export enum BuilderTag {
     Rare,
     Year2
 }
+
+export enum Franchise {
+    Dimensions,
+    BackToTheFuture,
+    TheSimpsons,
+    DoctorWho,
+    Portal2,
+    Ghostbusters,
+    MidwayArcade,
+    Ninjago,
+    ScoobyDoo,
+    JurassicWorld,
+    DCComics,
+    LegoMovie,
+    LordOfTheRings,
+    TheWizardOfOz,
+    Chima,
+    Ghostbusters2016,
+    MissionImpossible,
+    AdventureTime,
+    HarryPotter
+}
+
+export const franchises: string[] = [
+    'Lego Dimensions',
+    'Back To The Future',
+    'The Simpsons',
+    'Doctor Who',
+    'Portal 2',
+    'Ghostbusters',
+    'Midway Arcade',
+    'Ninjago',
+    'Scooby Doo',
+    'Jurassic World',
+    'DCComics',
+    'Lego Movie',
+    'Lord Of The Rings',
+    'The Wizard Of Oz',
+    'Chima',
+    'Ghostbusters 2016',
+    'Mission: Impossible',
+    'Adventure Time',
+    'Harry Potter' 
+];
