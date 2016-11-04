99a2ccd4391af8afebd658edc00721a9aa415ca9
Jakob Harder <jharder@live.com>
2016-11-04 15:56:21 +0100

    added wave 7 picture

--- src/app/data/static-waves.ts
+++ src/app/data/static-waves.ts
@@ -14,7 +14,7 @@ export const waves: WaveData[] = [
     { year: 1, wave: 4, release: "March 2016", released: true, image: null },
     { year: 1, wave: 5, release: "May 2016", released: true, image: null },
     { year: 2, wave: 6, release: "September 2016", released: true, image: 'wave-6' },
-    { year: 2, wave: 7, release: "November 2016", released: false, image: null },
+    { year: 2, wave: 7, release: "November 2016", released: false, image: 'wave-7' },
     { year: 2, wave: 7.5, release: "February 2017", released: false, image: 'wave-7.5' },
     { year: 2, wave: 9, release: "??", released: false, image: null },
 ];
