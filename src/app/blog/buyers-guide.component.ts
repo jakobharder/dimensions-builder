8a2133ccfd5d3c83a85dcdcfddc9db25e060b3d1
Jakob Harder <jharder@live.com>
2016-11-13 14:25:23 +0100

    add recommend packs to buyers guide

--- src/app/blog/buyers-guide.component.ts
+++ src/app/blog/buyers-guide.component.ts
@@ -19,6 +19,9 @@ export class BuyersGuideComponent implements OnInit {
     private teamComments: string[];
     private funPacks: Pack[];
     private funComments: string[];
+
+    private top3Packs: Pack[];
+    private top3Comments: string[];
     
     constructor(private meta: MetaService,
                 private data: DataService) {
@@ -66,5 +69,18 @@ export class BuyersGuideComponent implements OnInit {
             'Needed for Ninjago Adventure World and Pole Vault. You could use different Fun Packs to achieve the same.',
             'Needed for Dive + Ghost Immunity ability.',
             'Needed for Wizard of Oz Adventure World. If you do not care about 100% and the Adventure World, then the Harry Potter Team Pack is a better choice. Lord Voldemort has the same abilities and more as the Wicked Witch.'];
+        this.top3Packs = this.data.getPacks([71204, 71203, 71248, 71245, 71247, 71246, 71238, 71209, 71236, 71241, 71251]);
+        this.top3Comments = ['I absolutely recommended the Doctor Who Level Pack! In many levels and worlds the Tardis is used to get to secret areas giving you much play value outside of the actual level pack contents. It should be your first choice of Level Pack.',
+            'Personally, I find Portal 2 in the Lego world adorable. The Portal Gun and Weight Switch abilities are used here and there but not as much as the Tardis. So if you like Portal, then definitely go for this one.',
+            'Ethan Hunt is one of the few characters in Lego Dimensions who can do tons of things, even underwater! If plan to buy only a few Packs, then the Mission: Impossible Level Pack should help you to unlock many gold bricks.',
+            'Adventure Time is pretty fun in Lego Dimensions. Finn has also many abilities, but not as many as Ethan Hunt does. Alternatively, you can also consider the Jake and Lumpy Space Princess Team Pack to get at least the Adventure World.',
+            'Both Harry and Lord Voldemort can fly and have many other abilities including everything Gandalf and the Wicked Witch have. Diffindo and Parseltongue are new year 2 abilities used in other levels as well making this Team Pack a nice addition.',
+            'Jake comes with some rare abilities like Drilling, Digging, Sonar Smash - all in one character. If you don\'t have these yet, especially if don\'t plan to buy the Adventure Time Level Pack then this is a good choice. ',
+            'Cyberman has just tons of abilities. See the details for your self.',
+            'Wonder Woman or Superman are great, especially because of their Invincibilty and Flying abilities. The music when they fly is annoying though.',
+            'Wonder Woman or Superman are great, especially because of their Invincibilty and Flying abilities. The music when they fly is annoying though.',
+            'Slimer is yet another multi-talent who can also do many things underwater.',
+            'B.A. Baracus is a fun character and the best (and only) Fun Pack of year 2 so far.'
+            ]
     }
 }
