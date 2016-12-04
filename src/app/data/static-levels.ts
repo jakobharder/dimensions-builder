7c785660a988873f63c4f3dde7a3ffc1911c06e7
Jakob Harder <jharder@live.com>
2016-12-04 16:34:36 +0100

    add last update date to all levels

--- src/app/data/static-levels.ts
+++ src/app/data/static-levels.ts
@@ -32,7 +32,9 @@ export const levels: LevelData[] = [
         access: Ability.Level_AdventureTime, walkthrough: '', year: 2, franchise: Franchise.AdventureTime, 
         desc: 'If you play through this level you will gain Finn\'s Laser ability (Billy\'s Gauntlet). Also like with all Level packs, you have to play the level in order to get the build instructions for the Jakemobile and the Ancient War Elephant.',
         image: 'adventure-time-level',
-        fullVideo: '8WIV1MA-hhg' },
+        fullVideo: '8WIV1MA-hhg',
+        lastUpdate: 'Nov 13, 2016'
+    },
 
     <LevelData>{ url: 'mission-impossible-level', name: 'Mission: Impossible', chapter: 0, story: 'Year 2 Level Packs',
         abilitiesMinikits: [Ability.RelicDetector, Ability.Parseltongue, Ability.Laser, Ability.Intelligence, Ability.MiniAccess, Ability.SonarSmash, Ability.Boomerang, Ability.Digging, Ability.SlurpAccess, Ability.ChargeTransfer, Ability.MasterBuild, Ability.Magic], 
@@ -43,7 +45,9 @@ export const levels: LevelData[] = [
         desc: 'You have to play the level in order to get the build instructions for the IMF Scrambler and the IMF Sport Car.',
         image: 'mission-impossible-level',
         fullVideo: 'fG66-JlOJDE',
-        quickVideo: 'IvOCseETdKM' },
+        quickVideo: 'IvOCseETdKM',
+        lastUpdate: 'Nov 13, 2016'
+    },
     
     <LevelData>{ url: 'adventure-time-adventure-world', name: 'Adventure Time World', chapter: 0, story: 'Year 2 Adventure Worlds',
         abilitiesMinikits: [Ability.SwordSwitch, Ability.Laser, Ability.Target, Ability.Tardis, Ability.Magic, Ability.Technology, Ability.SonarSmash, Ability.SilverDestroy, Ability.RelicDetector, Ability.Grapple, Ability.FixIt, Ability.Accelerator, Ability.DivePlusLaser, Ability.SlurpAccess, 
@@ -54,7 +58,8 @@ export const levels: LevelData[] = [
         abilitiesStory: [], 
         access: Ability.Adventure_AdventureTime, walkthrough: '', year: 2, franchise: Franchise.AdventureTime, 
         desc: '',
-        },
+        lastUpdate: 'Nov 13, 2016'
+    },
     
     <LevelData>{ url: 'a-team-adventure-world', name: 'A-Team World', chapter: 0, story: 'Year 2 Adventure Worlds',
         abilitiesMinikits: [Ability.ChargeTransfer, Ability.SuperStrengthHandles, Ability.LaserDeflector, Ability.FixIt, Ability.ATeamMasterBuild, Ability.Accelerator, 
@@ -66,7 +71,8 @@ export const levels: LevelData[] = [
         abilitiesStory: [], 
         access: Ability.Adventure_ATeam, walkthrough: '', year: 2, franchise: Franchise.ATeam, 
         desc: '',
-        },
+        lastUpdate: 'Nov 13, 2016'
+    },
     
     <LevelData>{ url: 'ghostbusters-2016-adventure-world', name: 'Ghostbusters 2016 World', chapter: 0, story: 'Year 2 Adventure Worlds',
         abilitiesMinikits: [Ability.Laser, Ability.FixIt, Ability.SuperStrengthHandles, Ability.SuspendGhost, Ability.TrapGhosts, Ability.Intelligence, Ability.Magic,
@@ -78,7 +84,8 @@ export const levels: LevelData[] = [
         abilitiesStory: [], 
         access: Ability.Adventure_Ghostbusters2016, walkthrough: '', year: 2, franchise: Franchise.Ghostbusters2016, 
         desc: '',
-        },
+        lastUpdate: 'Nov 13, 2016'
+    },
     
     <LevelData>{ url: 'harry-potter-adventure-world', name: 'Harry Potter World', chapter: 0, story: 'Year 2 Adventure Worlds',
         abilitiesMinikits: [Ability.Magic, Ability.Intelligence, Ability.Acrobat, Ability.RelicDetector, Ability.SuspendGhost, Ability.TrapGhosts, Ability.HazardCleaner, 
@@ -89,7 +96,8 @@ export const levels: LevelData[] = [
         abilitiesStory: [], 
         access: Ability.Adventure_HarryPotter, walkthrough: '', year: 2, franchise: Franchise.HarryPotter, 
         desc: '',
-        },
+        lastUpdate: 'Nov 13, 2016'
+    },
     
     <LevelData>{ url: 'mission-impossible-adventure-world', name: 'Mission: Impossible World', chapter: 0, story: 'Year 2 Adventure Worlds',
         abilitiesMinikits: [Ability.Flying, Ability.MagnoGloves, Ability.Acrobat, Ability.FuseBox, Ability.Hacking, Ability.SonarSmash, Ability.FixIt, Ability.XRay, 
@@ -102,7 +110,8 @@ export const levels: LevelData[] = [
         abilitiesStory: [], 
         access: Ability.MissionImpossibleWorld, walkthrough: '', year: 2, franchise: Franchise.MissionImpossible, 
         desc: '',
-        },
+        lastUpdate: 'Nov 13, 2016'
+    },
 
     <LevelData>{ url: 'ghostbusters-1', name: 'Paranormal Beginnings', chapter: 1, story: 'Ghostbusters 2016',
         abilitiesMinikits: [Ability.RelicDetector, Ability.Grapple, Ability.Magic, Ability.SilverDestroy, Ability.ATeamMasterBuild, Ability.TimeTravel, Ability.MasterBuild, Ability.Diffindo], 
@@ -112,7 +121,8 @@ export const levels: LevelData[] = [
         access: Ability.Ghostbusters2016Story, year: 2, franchise: Franchise.Ghostbusters2016,
         desc: 'In the first level of the Ghostbusters 2016 Story Pack you play already with three characters Abby Yates, Jillian Holtzmann, Erin Gilbert. Patty Tolan will follow later. Notable is that you need the alternate character abilities - Fix-It and Technology.',
         image: 'ghostbusters-chapter1-thumbnail',
-        storyVideo: 'TAunMvBvtuc'
+        storyVideo: 'TAunMvBvtuc',
+        lastUpdate: 'Nov 13, 2016'
     },
 
     <LevelData>{ url: 'ghostbusters-2', name: 'Ghostbusting!', chapter: 2, story: 'Ghostbusters 2016',
@@ -120,7 +130,8 @@ export const levels: LevelData[] = [
         abilitiesRescue: [Ability.PoleVault, Ability.Acrobat], 
         abilitiesExtra: [Ability.FuseBox],
         abilitiesStory: [Ability.TowBar, Ability.Laser, Ability.PKEMeter, Ability.SuperStrengthHandles, Ability.FixIt, Ability.Technology, Ability.ChargeTransfer, Ability.Accelerator, Ability.SuspendGhost, Ability.TrapGhosts],
-        access: Ability.Ghostbusters2016Story, year: 2, franchise: Franchise.Ghostbusters2016
+        access: Ability.Ghostbusters2016Story, year: 2, franchise: Franchise.Ghostbusters2016,
+        lastUpdate: 'Nov 13, 2016'
     },
 
     <LevelData>{ url: 'ghostbusters-3', name: 'The Mercado Building', chapter: 3, story: 'Ghostbusters 2016',
@@ -128,7 +139,8 @@ export const levels: LevelData[] = [
         abilitiesRescue: [Ability.PortalGun], 
         abilitiesExtra: [],
         abilitiesStory: [Ability.Laser, Ability.TowBar, Ability.SuspendGhost, Ability.TrapGhosts, Ability.PKEMeter, Ability.FixIt, Ability.Intelligence, Ability.Accelerator, Ability.ChargeTransfer],
-        access: Ability.Ghostbusters2016Story, year: 2, franchise: Franchise.Ghostbusters2016
+        access: Ability.Ghostbusters2016Story, year: 2, franchise: Franchise.Ghostbusters2016,
+        lastUpdate: 'Nov 13, 2016'
     },
 
     <LevelData>{ url: 'ghostbusters-4', name: 'Breaking the Barrier', chapter: 4, story: 'Ghostbusters 2016',
@@ -136,7 +148,8 @@ export const levels: LevelData[] = [
         abilitiesRescue: [Ability.SonarSmash, Ability.MasterBuild, Ability.SilverDestroy], 
         abilitiesExtra: [],
         abilitiesStory: [Ability.SuspendGhost, Ability.TrapGhosts, Ability.SuperStrength, Ability.Intelligence, Ability.SuperStrengthHandles, Ability.ChargeTransfer, Ability.FixIt, Ability.PKEMeter, Ability.Laser, Ability.Accelerator],
-        access: Ability.Ghostbusters2016Story, year: 2, franchise: Franchise.Ghostbusters2016
+        access: Ability.Ghostbusters2016Story, year: 2, franchise: Franchise.Ghostbusters2016,
+        lastUpdate: 'Nov 13, 2016'
     },
 
     <LevelData>{ url: 'ghostbusters-5', name: 'The Battle for Times Square', chapter: 5, story: 'Ghostbusters 2016',
@@ -144,7 +157,8 @@ export const levels: LevelData[] = [
         abilitiesRescue: [Ability.FuseBox, Ability.VineCutting], 
         abilitiesExtra: [],
         abilitiesStory: [Ability.SuperStrength, Ability.SuperStrengthHandles, Ability.ChargeTransfer, Ability.Accelerator, Ability.SuspendGhost, Ability.TrapGhosts, Ability.PKEMeter, Ability.Intelligence, Ability.FixIt, Ability.Technology, Ability.Laser, Ability.Grapple],
-        access: Ability.Ghostbusters2016Story, year: 2, franchise: Franchise.Ghostbusters2016
+        access: Ability.Ghostbusters2016Story, year: 2, franchise: Franchise.Ghostbusters2016,
+        lastUpdate: 'Nov 13, 2016'
     },
 
     <LevelData>{ url: 'ghostbusters-6', name: 'The Final Showdown', chapter: 6, story: 'Ghostbusters 2016',
@@ -152,7 +166,8 @@ export const levels: LevelData[] = [
         abilitiesRescue: [], 
         abilitiesExtra: [],
         abilitiesStory: [Ability.SilverDestroy, Ability.SuperStrengthHandles, Ability.Laser, Ability.Grapple, Ability.FixIt, Ability.RopeSwings, Ability.SuspendGhost, Ability.TrapGhosts, Ability.ChargeTransfer, Ability.TowBar, Ability.TargetPlusSilver, Ability.PKEMeter, Ability.Technology],
-        access: Ability.Ghostbusters2016Story, year: 2, franchise: Franchise.Ghostbusters2016
+        access: Ability.Ghostbusters2016Story, year: 2, franchise: Franchise.Ghostbusters2016,
+        lastUpdate: 'Nov 13, 2016'
     },
 
     <LevelData>{ url: 'back-to-the-future-level', name: 'Back to the Future', chapter: 0, story: 'Year 1 Level Packs',
@@ -160,21 +175,25 @@ export const levels: LevelData[] = [
         abilitiesStory: [Ability.SonarSmash, Ability.Accelerator, Ability.TimeTravel, Ability.HoverOrFlying, Ability.BackToTheFutureLevel], 
         access: Ability.BackToTheFutureLevel, walkthrough: 'https://www.youtube.com/watch?v=6X6F7J2aZxo', year: 1, franchise: Franchise.BackToTheFuture,
         desc: undefined,
-        image: 'back-to-the-future-level' }, /* Marty McFly, car */
+        image: 'back-to-the-future-level',
+        lastUpdate: 'Nov 13, 2016' }, /* Marty McFly, car */
 
     <LevelData>{ url: 'portal-2-level', name: 'Aperture Science', chapter: 0, story: 'Year 1 Level Packs',
         abilitiesMinikits: [Ability.LaserDeflector, Ability.XRay, Ability.Flying, Ability.Target, Ability.Grapple, Ability.Boomerang, Ability.Illumination, Ability.RelicDetector, Ability.Laser, Ability.VineCutting], abilitiesRescue: [Ability.Chi], abilitiesExtra: [], abilitiesStory: [Ability.PortalGun, Ability.WeightSwitch, Ability.SentryTurret], 
-        access: Ability.Level_Portal2, walkthrough: '', year: 1, franchise: Franchise.Portal2 }, /* chell, turret */
+        access: Ability.Level_Portal2, walkthrough: '', year: 1, franchise: Franchise.Portal2,
+        lastUpdate: 'Nov 13, 2016' }, /* chell, turret */
 
     <LevelData>{ url: 'doctor-who-level', name: 'The Dalek Extermination of Earth', chapter: 0, story: 'Year 1 Level Packs',
         abilitiesMinikits: [Ability.VineCutting, Ability.RelicDetector, Ability.Grapple, Ability.Magic, Ability.Digging, Ability.Laser, Ability.MasterBuild, Ability.Boomerang, Ability.Stealth, Ability.Electricity], abilitiesRescue: [Ability.WaterSpray, Ability.Magic], abilitiesExtra: [], abilitiesStory: [Ability.FixIt, Ability.Tardis, Ability.SilverDestroy, Ability.Technology, Ability.Stealth, Ability.Flying], 
-        access: Ability.Level_DoctorWho, walkthrough: '', year: 1, franchise: Franchise.DoctorWho }, /* water spray or ice */
+        access: Ability.Level_DoctorWho, walkthrough: '', year: 1, franchise: Franchise.DoctorWho,
+        lastUpdate: 'Nov 13, 2016' }, /* water spray or ice */
 
     <LevelData>{ url: 'the-simpsons-level', name: 'The Mysterious Voyage of Homer', chapter: 0, story: 'Year 1 Level Packs',
         abilitiesMinikits: [Ability.Tracking, Ability.RelicDetector, Ability.Laser, Ability.Hacking, Ability.Tardis, Ability.Technology, Ability.PortalGun /* or fly */, Ability.WeightSwitch], abilitiesRescue: [Ability.Atlantis], abilitiesExtra: [Ability.Gyrosphere, Ability.Chi, Ability.PoleVault, Ability.Acrobat], 
         abilitiesStory: [Ability.SonarSmash, Ability.SimpsonsLevel, Ability.TowBar, Ability.SuperStrength, Ability.SilverDestroy, Ability.Magic, Ability.Accelerator /* vehicle */, Ability.TauntEnemies], 
         access: Ability.SimpsonsLevel, walkthrough: '', year: 1, franchise: Franchise.TheSimpsons,
-        image: 'simpsons-level' },
+        image: 'simpsons-level',
+        lastUpdate: 'Nov 13, 2016' },
         /* extra gyrosphete, target, chima, poul valt, acrobat */
         /* atlantis is for rescue? */
 
@@ -183,14 +202,16 @@ export const levels: LevelData[] = [
         abilitiesStory: [Ability.Laser, Ability.Accelerator, Ability.SuspendGhost, Ability.TrapGhosts, Ability.GhostbustersLevel], 
         access: Ability.GhostbustersLevel, walkthrough: '', year: 1, franchise: Franchise.Ghostbusters,
         desc: 'You have to play the level in order to get the build instructions for the Ecto-1 and the Ghost Trap.',
-        image: 'ghostbusters-level' },
+        image: 'ghostbusters-level',
+        lastUpdate: 'Nov 13, 2016' },
 
     <LevelData>{ url: 'midway-arcade-level', name: 'Retro Wreckage', chapter: 0, story: 'Year 1 Level Packs',
         abilitiesMinikits: [Ability.Grapple, Ability.XRay, Ability.SonarSmash, Ability.Drone, Ability.FlightDock, Ability.Magic, Ability.RelicDetector, Ability.FixIt, Ability.CargoHook], abilitiesRescue: [Ability.Digging], abilitiesExtra: [Ability.TowBar, Ability], 
         abilitiesStory: [Ability.Invulnerability, Ability.MidwayArcadeLevel, Ability.Accelerator, Ability.Arcade, Ability.Invisibility, Ability.Laser, Ability.Speed, Ability.SuperStrength, Ability.Stealth], 
         access: Ability.MidwayArcadeLevel, walkthrough: '', year: 1, franchise: Franchise.MidwayArcade,
         desc: 'You have to play the level in order to get the build instructions for the Arcade Machine and the Spy Hunter.',
-        image: 'midway-arcade-level' },
+        image: 'midway-arcade-level',
+        lastUpdate: 'Nov 13, 2016' },
         
     <LevelData>{ url: 'main-story-1-follow-the-lego-brick-road', name: 'Follow The Lego Brick Road – Wizard Of Oz', chapter: 1, story: 'Main Story',
         abilitiesMinikits: [Ability.SilverDestroy, Ability.Hacking, Ability.HazardCleaner, Ability.Grapple, Ability.RelicDetector, Ability.SuperStrength, Ability.Tracking, 
@@ -198,7 +219,8 @@ export const levels: LevelData[] = [
         abilitiesExtra: [Ability.Arcade], abilitiesStory: [Ability.Grapple, Ability.Magic, Ability.RelicDetector, Ability.Accelerator, Ability.Boomerang,
         Ability.Acrobat],
         // ranged attack (magic or boomerang)? // driving vehicle or flying
-        access: -1, walkthrough: 'http://www.ign.com/wikis/lego-dimensions/Follow_the_LEGO_Brick_Road', year: 1, franchise: Franchise.Dimensions },
+        access: -1, walkthrough: 'http://www.ign.com/wikis/lego-dimensions/Follow_the_LEGO_Brick_Road', year: 1, franchise: Franchise.Dimensions,
+        lastUpdate: 'Nov 13, 2016' },
 
     <LevelData>{ url: 'main-story-2-meltdown-at-sector-7g', name: 'Meltdown at Sector 7-G – The Simpsons', chapter: 2, story: 'Main Story',
         abilitiesMinikits: [Ability.Digging, /* Ability.Shoot */ Ability.SilverDestroy, Ability.Drilling, Ability.Spinjitzu, Ability.Illumination, Ability.WeightSwitch,
