53e195270ccedbf21786dd879f40d01db7de1434
Jakob Harder <jharder@live.com>
2017-01-20 18:56:17 +0100

    fix amazon ads

--- src/app/components/amazon.component.ts
+++ src/app/components/amazon.component.ts
@@ -18,12 +18,8 @@ export class AmazonComponent implements AfterViewInit {
 
 	ngAfterViewInit() {
 		try {
-			let node = document.createElement('script');
-			node.src = '//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=4571b6bb-3fbf-404a-989d-a3478d7891e0&storeId=dimensionsbui-20';
-			node.type = 'text/javascript';
-			node.async = true;
-			node.charset = 'utf-8';
-			document.getElementById('amazon-box').appendChild(node);
+			require('//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=4571b6bb-3fbf-404a-989d-a3478d7891e0&storeId=dimensionsbui-20');
+			
 		} catch (e) {
 		}
 	}
